37b04d7783e00d851a2000feabcaf5105171c7e1
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-02-26 13:55:28 +0100

    Check for error in parsing the yaml file

--- src/utils.cpp
+++ src/utils.cpp
@@ -14,19 +14,41 @@
 #include <fstream>
 #include <odri_control_interface/common.hpp>
 #include <odri_control_interface/utils.hpp>
+#include <sstream>
 
 namespace odri_control_interface
 {
-inline bool file_exists(const std::string& name)
-{
-    std::ifstream f(name.c_str());
-    return f.good();
-}
+#define assert_yaml_parsing(yaml_node, parent_node_name, child_node_name)      \
+    if (!yaml_node[child_node_name])                                           \
+    {                                                                          \
+        std::ostringstream oss;                                                \
+        oss << "Error: Wrong parsing of the YAML file from src file: ["        \
+            << __FILE__ << "], in function: [" << __FUNCTION__ << "], line: [" \
+            << __LINE__ << ". Node [" << child_node_name                       \
+            << "] does not exists under the node [" << parent_node_name        \
+            << "].";                                                           \
+        throw std::runtime_error(oss.str());                                   \
+    }                                                                          \
+    assert(true)
+
+#define assert_file_exists(filename)                                    \
+    std::ifstream f(filename.c_str());                                  \
+    if (!f.good())                                                      \
+    {                                                                   \
+        std::ostringstream oss;                                         \
+        oss << "Error: Problem opening the file [" << filename          \
+            << "], from src file: [" << __FILE__ << "], in function: [" \
+            << __FUNCTION__ << "], line: [" << __LINE__                 \
+            << ". The file may not exists.";                            \
+        throw std::runtime_error(oss.str());                            \
+    }                                                                   \
+    assert(true)
 
 std::shared_ptr<JointModules> JointModulesFromYaml(
     std::shared_ptr<MasterBoardInterface> robot_if,
     const YAML::Node& joint_modules_yaml)
 {
+    assert_yaml_parsing(joint_modules_yaml, "joint_modules", "motor_numbers");
     const YAML::Node& motor_numbers = joint_modules_yaml["motor_numbers"];
     std::size_t n = motor_numbers.size();
     VectorXi motor_numbers_vec;
@@ -36,11 +58,14 @@ std::shared_ptr<JointModules> JointModulesFromYaml(
         motor_numbers_vec(i) = motor_numbers[i].as<int>();
     }
 
+    assert_yaml_parsing(
+        joint_modules_yaml, "joint_modules", "reverse_polarities");
     const YAML::Node& rev_polarities = joint_modules_yaml["reverse_polarities"];
     if (rev_polarities.size() != n)
     {
         throw std::runtime_error(
-            "Motor polarities has different size than motor numbers");
+            "Motor polarities has different size than motor "
+            "numbers");
     }
     VectorXb rev_polarities_vec;
     rev_polarities_vec.resize(n);
@@ -49,12 +74,15 @@ std::shared_ptr<JointModules> JointModulesFromYaml(
         rev_polarities_vec(i) = rev_polarities[i].as<bool>();
     }
 
+    assert_yaml_parsing(
+        joint_modules_yaml, "joint_modules", "lower_joint_limits");
     const YAML::Node& lower_joint_limits =
         joint_modules_yaml["lower_joint_limits"];
     if (lower_joint_limits.size() != n)
     {
         throw std::runtime_error(
-            "Lower joint limits has different size than motor numbers");
+            "Lower joint limits has different size than motor "
+            "numbers");
     }
     VectorXd lower_joint_limits_vec;
     lower_joint_limits_vec.resize(n);
@@ -63,12 +91,15 @@ std::shared_ptr<JointModules> JointModulesFromYaml(
         lower_joint_limits_vec(i) = lower_joint_limits[i].as<double>();
     }
 
+    assert_yaml_parsing(
+        joint_modules_yaml, "joint_modules", "upper_joint_limits");
     const YAML::Node& upper_joint_limits =
         joint_modules_yaml["upper_joint_limits"];
     if (upper_joint_limits.size() != n)
     {
         throw std::runtime_error(
-            "Upper joint limits has different size than motor numbers");
+            "Upper joint limits has different size than motor "
+            "numbers");
     }
     VectorXd upper_joint_limits_vec;
     upper_joint_limits_vec.resize(n);
@@ -77,6 +108,12 @@ std::shared_ptr<JointModules> JointModulesFromYaml(
         upper_joint_limits_vec(i) = upper_joint_limits[i].as<double>();
     }
 
+    assert_yaml_parsing(joint_modules_yaml, "joint_modules", "motor_constants");
+    assert_yaml_parsing(joint_modules_yaml, "joint_modules", "gear_ratios");
+    assert_yaml_parsing(joint_modules_yaml, "joint_modules", "max_currents");
+    assert_yaml_parsing(
+        joint_modules_yaml, "joint_modules", "max_joint_velocities");
+    assert_yaml_parsing(joint_modules_yaml, "joint_modules", "safety_damping");
     return std::make_shared<JointModules>(
         robot_if,
         motor_numbers_vec,
@@ -93,6 +130,7 @@ std::shared_ptr<JointModules> JointModulesFromYaml(
 std::shared_ptr<IMU> IMUFromYaml(std::shared_ptr<MasterBoardInterface> robot_if,
                                  const YAML::Node& imu_yaml)
 {
+    assert_yaml_parsing(imu_yaml, "imu", "rotate_vector");
     const YAML::Node& rotate_vector = imu_yaml["rotate_vector"];
     VectorXl rotate_vector_vec;
     rotate_vector_vec.resize(3);
@@ -105,6 +143,7 @@ std::shared_ptr<IMU> IMUFromYaml(std::shared_ptr<MasterBoardInterface> robot_if,
         rotate_vector_vec(i) = rotate_vector[i].as<long>();
     }
 
+    assert_yaml_parsing(imu_yaml, "imu", "orientation_vector");
     const YAML::Node& orientation_vector = imu_yaml["orientation_vector"];
     VectorXl orientation_vector_vec;
     orientation_vector_vec.resize(4);
@@ -124,6 +163,7 @@ std::shared_ptr<IMU> IMUFromYaml(std::shared_ptr<MasterBoardInterface> robot_if,
 std::shared_ptr<JointCalibrator> JointCalibratorFromYaml(
     std::shared_ptr<JointModules> joints, const YAML::Node& joint_calibrator)
 {
+    assert_yaml_parsing(joint_calibrator, "joint_calibrator", "search_methods");
     std::vector<CalibrationMethod> calib_methods;
     for (std::size_t i = 0; i < joint_calibrator["search_methods"].size(); i++)
     {
@@ -151,6 +191,8 @@ std::shared_ptr<JointCalibrator> JointCalibratorFromYaml(
         }
     }
 
+    assert_yaml_parsing(
+        joint_calibrator, "joint_calibrator", "position_offsets");
     const YAML::Node& position_offsets = joint_calibrator["position_offsets"];
     std::size_t n = position_offsets.size();
     VectorXd position_offsets_vec;
@@ -160,6 +202,10 @@ std::shared_ptr<JointCalibrator> JointCalibratorFromYaml(
         position_offsets_vec(i) = position_offsets[i].as<double>();
     }
 
+    assert_yaml_parsing(joint_calibrator, "joint_calibrator", "Kp");
+    assert_yaml_parsing(joint_calibrator, "joint_calibrator", "Kd");
+    assert_yaml_parsing(joint_calibrator, "joint_calibrator", "T");
+    assert_yaml_parsing(joint_calibrator, "joint_calibrator", "dt");
     return std::make_shared<JointCalibrator>(
         joints,
         calib_methods,
@@ -173,15 +219,11 @@ std::shared_ptr<JointCalibrator> JointCalibratorFromYaml(
 std::shared_ptr<Robot> RobotFromYamlFile(const std::string& if_name,
                                          const std::string& file_path)
 {
-    if (!file_exists(file_path))
-    {
-        throw std::runtime_error("The YAML parameter file [" + file_path +
-                                 "] does not exists.");
-    }
-
+    assert_file_exists(file_path);
     YAML::Node param = YAML::LoadFile(file_path);
 
     // Parse the robot part.
+    assert_yaml_parsing(param, file_path, "robot");
     const YAML::Node& robot_node = param["robot"];
 
     // 1. Create the robot interface.
@@ -189,9 +231,11 @@ std::shared_ptr<Robot> RobotFromYamlFile(const std::string& if_name,
         std::make_shared<MasterBoardInterface>(if_name);
 
     // 2. Create the joint modules.
+    assert_yaml_parsing(robot_node, "robot", "joint_modules");
     std::shared_ptr<JointModules> joints =
         JointModulesFromYaml(robot_if, robot_node["joint_modules"]);
 
+    assert_yaml_parsing(robot_node, "robot", "imu");
     std::shared_ptr<IMU> imu = IMUFromYaml(robot_if, robot_node["imu"]);
 
     // 3. Create the robot instance from the objects.
@@ -200,15 +244,12 @@ std::shared_ptr<Robot> RobotFromYamlFile(const std::string& if_name,
 
 std::shared_ptr<Robot> RobotFromYamlFile(const std::string& file_path)
 {
-    if (!file_exists(file_path))
-    {
-        throw std::runtime_error("The YAML parameter file [" + file_path +
-                                 "] does not exists.");
-    }
-    
+    assert_file_exists(file_path);
     YAML::Node param = YAML::LoadFile(file_path);
 
     // Parse the robot part.
+    assert_yaml_parsing(param, file_path, "robot");
+    assert_yaml_parsing(param["robot"], "robot", "interface");
     std::string if_name = param["robot"]["interface"].as<std::string>();
 
     return RobotFromYamlFile(if_name, file_path);
@@ -217,8 +258,10 @@ std::shared_ptr<Robot> RobotFromYamlFile(const std::string& file_path)
 std::shared_ptr<JointCalibrator> JointCalibratorFromYamlFile(
     const std::string& file_path, std::shared_ptr<JointModules> joints)
 {
+    assert_file_exists(file_path);
     YAML::Node param = YAML::LoadFile(file_path);
 
+    assert_yaml_parsing(param, file_path, "joint_calibrator");
     return JointCalibratorFromYaml(joints, param["joint_calibrator"]);
 }
 
