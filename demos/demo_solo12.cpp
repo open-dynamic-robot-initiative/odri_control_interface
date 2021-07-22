afe897754cd33af87a2c89bbab583fef0b7328d5
Julian Viereck <jv1439@nyu.edu>
2021-07-22 14:17:32 -0400

    Update the demo_solo12.cpp to match more the python version

--- demos/demo_solo12.cpp
+++ demos/demo_solo12.cpp
@@ -17,72 +17,50 @@ int main()
     // Define the robot from a yaml file.
     auto robot = RobotFromYamlFile(CONFIG_SOLO12_YAML);
 
-    // Start the robot.
-    robot->Start();
-
-    // Define controller to calibrate the joints from yaml file.
-    auto calib_ctrl = JointCalibratorFromYamlFile(
-        CONFIG_SOLO12_YAML, robot->joints);
+    // Initialize the communication, session, joints, wait for motors to be ready
+    // and run the joint calibration.ArithmeticError()
+    robot->Initialize();
 
     // Initialize simple pd controller.
     Vector12d torques;
-    double kp = 3.;
-    double kd = 0.05;
+    double kp = 5.;
+    double kd = 0.1;
     int c = 0;
-    std::chrono::time_point<std::chrono::system_clock> last =
-        std::chrono::system_clock::now();
     bool is_calibrated = false;
-    while (!robot->IsTimeout())
+    while (!robot->IsTimeout() && !robot->HasError())
     {
-        if (((std::chrono::duration<double>)(std::chrono::system_clock::now() -
-                                             last))
-                .count() > 0.001)
+        robot->ParseSensorData();
+
+        // Run the main controller.
+        auto pos = robot->joints->GetPositions();
+        auto vel = robot->joints->GetVelocities();
+
+        // Compute PD control on the zero position.
+        for (int i = 0; i < 12; i++)
         {
-            last = std::chrono::system_clock::now();  // last+dt would be better
-            robot->ParseSensorData();
-
-            if (robot->IsReady())
-            {
-                if (!is_calibrated)
-                {
-                    is_calibrated = calib_ctrl->Run();
-                    if (is_calibrated)
-                    {
-                        std::cout << "Calibration is done." << std::endl;
-                    }
-                }
-                else
-                {
-                    // Run the main controller.
-                    auto pos = robot->joints->GetPositions();
-                    auto vel = robot->joints->GetVelocities();
-                    // Reverse the positions;
-                    for (int i = 0; i < 12; i++)
-                    {
-                        torques[i] = -kp * pos[i] - kd * vel[i];
-                    }
-                    robot->joints->SetTorques(torques);
-                }
-            }
-
-            // Checks if the robot is in error state (that is, if any component
-            // returns an error). If there is an error, the commands to send
-            // are changed to send the safety control.
-            robot->SendCommand();
-
-            c++;
-            if (c % 1000 == 0)
-            {
-                std::cout << "Joints: ";
-                robot->joints->PrintVector(robot->joints->GetPositions());
-                std::cout << std::endl;
-            }
+            torques[i] = kp * (0 - pos[i]) - kd * vel[i];
         }
-        else
+        robot->joints->SetTorques(torques);
+
+        // Checks if the robot is in error state (that is, if any component
+        // returns an error). If there is an error, the commands to send
+        // are changed to send the safety control.
+        robot->SendCommandAndWaitEndOfCycle(0.001);
+
+        c++;
+        if (c % 1000 == 0)
         {
-            std::this_thread::yield();
+            std::cout << "Joints: ";
+            robot->joints->PrintVector(robot->joints->GetPositions());
+            std::cout << std::endl;
         }
     }
-    std::cout << "Normal program shutdown." << std::endl;
+
+    if (robot->IsTimeout()) {
+        printf("Timeout detected\n");
+    } else if (robot->HasError()) {
+        printf("Error detected\n");
+    }
+
     return 0;
 }
