b4b21150e088ddfa9fb7cb80b45b0706198102a9
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2023-08-28 13:38:23 +0200

    fixup! fixup! Add ErrorMessage class

--- include/odri_control_interface/error.hpp
+++ include/odri_control_interface/error.hpp
@@ -38,28 +38,53 @@ public:
 // updated to C++20, it might be worth revisiting this and put back the
 // concept/requires-part of the answer linked above.
 
-using MessageParam = std::variant<int, double>;
+using MessageArgument = std::variant<int, double>;
 
-// FIXME: rename variables and maybe clean up a bit
-// FIXME: can I get the argument limit working?
-// FIXME: write some doxygen
 // FIXME: actually use it instead of the classes based on Error.  remove the
 //        latter
-template <int MAX_PARAM>
+
+/**
+ * @brief Generic message class with lazy message construction.
+ *
+ * The constructor only takes a format string (for the fmt library) as
+ * string_view and arguments for it but does not directly construct the actual
+ * message from it.  This means no dynamic memory allocation is happening during
+ * construction (so safe to use in real-time critical code). The actual message
+ * string is only generated when calling @ref get_message.
+ *
+ * Based on https://stackoverflow.com/a/76944227 by Aedoro, CC BY-SA 4.0
+ *
+ * @tparam MAX_ARGS Maximum number of arguments that can be used in the format
+ * string.
+ */
+template <int MAX_ARGS>
 class Message
 {
 public:
+    /**
+     * @brief Constructor
+     *
+     * @param format Format string with '{}' placeholders (will be processed
+     *      with the fmt library, see there for more information).
+     * @param args Arguments for the format string.  Only types that are
+     *      convertible to MessageArgument are supported.
+     */
     template <typename... Args,
-             // ensure number or arguments does not exceed MAX_PARAM
-             // https://stackoverflow.com/a/39621288/2095383
+             // ensure number or arguments does not exceed MAX_ARGS
+             // https://stackoverflow.com/a/39621288
              // NOTE: With C++20, it would be better to use `requires`
-              std::enable_if_t<(sizeof...(Args) <= MAX_PARAM)>* = nullptr>
+              std::enable_if_t<(sizeof...(Args) <= MAX_ARGS)>* = nullptr>
     Message(std::string_view format, Args... args)
-        : m_format(format), m_nr_params(sizeof...(args)), m_params{args...}
+        : format_(format), num_args_(sizeof...(args)), args_{args...}
     {
     }
 
-    auto get_message() const
+    /**
+     * @brief Construct the message from format string and arguments.
+     *
+     * This involves dynamic memory allocation so it is not real-time safe!
+     */
+    std::string get_message() const
     {
         // constructing an argument list for fmt::format from an array is a bit
         // of a pain... based on https://stackoverflow.com/a/59744762
@@ -69,7 +94,7 @@ public:
 
         using fmt_ctx = fmt::format_context;
         std::vector<fmt::basic_format_arg<fmt_ctx>> fmt_args;
-        for (auto const& a : m_params)
+        for (auto const& a : args_)
         {
             if (std::holds_alternative<double>(a))
             {
@@ -84,14 +109,17 @@ public:
         }
 
         return fmt::vformat(
-            m_format,
+            format_,
             fmt::basic_format_args<fmt_ctx>(fmt_args.data(), fmt_args.size()));
     }
 
 private:
-    std::string_view m_format;
-    size_t m_nr_params;
-    std::array<MessageParam, MAX_PARAM> m_params;
+    //! Format string for the message (will be filled with args_ using fmt)
+    std::string_view format_;
+    //! Number of arguments that where passed to the constructor.
+    size_t num_args_;
+    //! Message arguments.  Elements with index >= num_args_ are undefined.
+    std::array<MessageArgument, MAX_ARGS> args_;
 };
 
 // Allow up to three arguments for error messages (increase number if needed)
