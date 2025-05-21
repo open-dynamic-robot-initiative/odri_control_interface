1ca239e4b6afeba2c6176e46d293e4bb15139aec
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-21 17:23:11 +0200

    nix: get package version dynamically
    see https://github.com/Gepetto/nix-lib/pull/1

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:Gepetto/nix-lib";
 
     odri-masterboard-sdk = {
       # FIXME update after https://github.com/open-dynamic-robot-initiative/master-board/pull/173
@@ -10,32 +11,37 @@
   };
 
   outputs =
-    { nixpkgs, odri-masterboard-sdk, ... }:
+    {
+      nixpkgs,
+      utils,
+      odri-masterboard-sdk,
+      ...
+    }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       sdk = odri-masterboard-sdk.packages.x86_64-linux.default;
+      rosVersion = utils.lib.rosVersion pkgs;
     in
     {
-      packages.x86_64-linux.default =
-        pkgs.stdenv.mkDerivation rec {
-          pname = "odri-control";
-          version = "1.0.1";
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation rec {
+        pname = "odri-control";
+        version = rosVersion ./package.xml;
 
-          src = builtins.path {
-            name = pname;
-            path = ./.;
-          };
-
-          nativeBuildInputs =
-            [ sdk ]
-            ++ (with pkgs; [
-              cmake
-              yaml-cpp
-              eigen
-              python312Packages.eigenpy
-              python312Packages.boost
-              python312
-            ]);
+        src = builtins.path {
+          name = pname;
+          path = ./.;
         };
+
+        nativeBuildInputs =
+          [ sdk ]
+          ++ (with pkgs; [
+            cmake
+            yaml-cpp
+            eigen
+            python312Packages.eigenpy
+            python312Packages.boost
+            python312
+          ]);
+      };
     };
 }
